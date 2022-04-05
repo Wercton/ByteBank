d65efc11456c4e3e0114ce160a598c925288b96c
Wercton Barbosa <werctonmatheus@gmail.com>
2022-04-05 13:10:31 -0300

    Refactor: classes creation for better legibility

--- lib/main.dart
+++ lib/main.dart
@@ -4,30 +4,54 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          backgroundColor: Color(0xff00BCD1),
-          body: Column(
-            children: [
-              Card(
-                child: ListTile(
-                  leading:  const Icon(Icons.monetization_on_outlined),
-                  title: Text('666,69'),
-                  subtitle: Text('Devil bread'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: const Icon(Icons.monetization_on_outlined),
-                  title: Text('666,69'),
-                  subtitle: Text('Devil bread'),
-                ),
-              ),
-            ],
-          ),
-          appBar: AppBar(title: Text('ByteBank')),
+          backgroundColor: const Color(0xff00BCD1),
+          body: ListTransfer(),
+          appBar: AppBar(title: const Text('ByteBank')),
           floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
+            child: const Icon(Icons.add),
             onPressed: () {},
           ),
         ),
       ),
     );
+
+class ListTransfer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: [
+        CardTransferInfo(Transfer(666.69, 'Devil bread')),
+        CardTransferInfo(Transfer(42.00, 'The answer, my friend.')),
+        CardTransferInfo(Transfer(7.00, 'The Old and the New')),
+      ],
+    );
+  }
+}
+
+class CardTransferInfo extends StatelessWidget {
+
+  final Transfer _transfer;
+
+  CardTransferInfo(this._transfer);  // alt + ins
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on_outlined),
+        title: Text(_transfer.value.toString()),
+        subtitle: Text(_transfer.transferDescription),
+      ),
+    );
+  }
+
+}
+
+class Transfer {
+  final double value;
+  final String transferDescription;
+
+  Transfer(this.value, this.transferDescription);
+}
